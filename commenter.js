6fd1d1aefaae3c93683284b49052eae824e44470
Aniko Fejes <oranjje@gmail.com>
2018-04-15 21:35:33 +0200

    feat(commenter): upvote/downvote

--- commenter.js
+++ commenter.js
@@ -16,20 +16,25 @@ const comments = [
   {
     author: 'Michael J Szymanski',
     comment: 'Interesting article',
+    votes: 0,
   },
   {
     author: 'Cory J Harris',
     comment: 'yes',
+    votes: 0,
   },
 ];
 const $commentContainer = document.querySelector('#commentContainer');
 const buildComments = () => {
   let commentHTML = '';
-  comments.forEach((comment) => {
+  comments.forEach((comment, index) => {
     commentHTML += `
       <li>
         <h3>${comment.author}</h3>
         <p>${comment.comment}</p>
+        <button data-action="up" data-id="${index}">+</button>
+        <button data-action="down" data-id="${index}">-</button>
+        <p>${comment.votes}</p>
       </li>
     `;
   })
@@ -49,5 +54,17 @@ $submit.addEventListener('click', () => {
   })
   $author.value = '';
   $comment.value = '';
+  buildComments();
+});
+
+// Upvote / downvote
+$commentContainer.addEventListener('click', (e) => {
+  const action = e.target.dataset.action;
+  const commentID = e.target.dataset.id;
+  if (action === 'up') {
+    comments[commentID].votes++;
+  } else if (action === 'down') {
+    comments[commentID].votes--;
+  }
   buildComments();
 });
