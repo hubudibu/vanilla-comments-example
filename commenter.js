4c1ea7129e3791bf609386c8f8a2990ea4d3351a
Aniko Fejes <oranjje@gmail.com>
2018-04-15 21:11:08 +0200

    feat(commenter): fetch article from api

--- commenter.js
+++ commenter.js
@@ -1,1 +1,12 @@
 console.log('it works');
+
+const $title = document.querySelector('#title');
+const $article = document.querySelector('#articleBody');
+
+// Load article from newsAPI
+fetch('https://newsapi.org/v2/everything?q=javascript&apiKey=e82d6427ef8949058f4ef4d050daa662')
+  .then(response => response.json())
+  .then((articles) => {
+    $title.textContent = articles.articles[0].title;
+    $article.textContent = articles.articles[0].description;
+  });
